ee190a6d6d44e96353315ea3b5770903c12b01a3
Spencer Heywood <heywoodlh@nix-macbook-air.tail5da4.ts.net>
2023-08-30 19:55:36 -0600

    updated argument

--- git/flake.nix
+++ git/flake.nix
@@ -9,20 +9,6 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         vim = vim-flake.defaultPackage.${system};
-        onepassword_path = if pkgs.stdenv.isDarwin then
-        "/Applications/1Password.app/Contents/MacOS/op-ssh-sign"
-        else
-        "/home/heywoodlh/.nix-profile/bin/op-ssh-sign";
-        onepassword_config = ''
-[gpg "ssh"]
-  program = "${onepassword_path}"
-
-[commit]
-  gpgsign = "true"
-
-[user]
-  signingkey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCYn+7oSNHXN3qqDDidw42Vv7fDS0iEpYqaa0wCXRPBlfWAnD81f6dxj/QPGfZtxpl9jvk7nAKpE7RVUvQiJzUC2VM3Bw/4ucT+xliEHo3oesMQQa1AT70VPTbP5PdU7oUpgQWLq39j9XHno2YPJ/WWtuOl/UTjY6IDokkAmNmvft/jqqkiwSkGMmw68qrLFEM7+rNwJV5cXKvvpB6Gqc7qnbJmk1TZ1MRGW5eLjP9ofDqiyoLbnTm7Dw3iHn40GgTcnv5CWGpa0vrKnnLEGrgRB7kR/pyvfsjapkHz0PDvuinQov+MgJfV8B8PHdPC94dsS0DEWJplxhYojtsYa1VZy5zTEMNWICz1QG1yKHN1JQtpbEreHG6DVYvqwnKvK/XN5yiEeiamhD2oKnSh36PexIR0h0AAPO29Ln+anqpRlqJ0nET2CNS04e0vpV4VDJrG6BnyGUQ6CCo7THSq97F4Ne0nY9fpYu5WTFTCh1tTm+nSey0fP/xk22oINl/41VTI/Vk5pNQuuhHUvQupJHw9cD74aKzRddwvgfuAQjPlEuxxsqgFTltTiPF6lZQNeoMIc1OMCRsnl1xNqIepnb7Q5O1CGq+BqtOWh3G4/SPQI5ZUIkOAZegsnPpGWYMrRd7s6LJn5LrBYaY6IvRxmpGOig3tjOUy3fqk7coyTeJXmQ=="
-        '';
         gitconfig = pkgs.writeText "gitconfig" ''
 [color]
   ui = "auto"
@@ -52,21 +38,20 @@
 [push]
   default = "simple"
 
-[user]
-  email = "github@heywoodlh.io"
-  name = "Spencer Heywood"
-
 [include]
   path = ~/.gitconfig
   path = ~/.config/git/config
 
-${onepassword_config}
+[user]
+  email = "github@heywoodlh.io"
+  name = "Spencer Heywood"
+
         '';
       in {
         packages = rec {
           git = pkgs.writeShellScriptBin "git" ''
             export GIT_CONFIG_GLOBAL=${gitconfig}
-            ${pkgs.git}/bin/git $@
+            ${pkgs.git}/bin/git "$@"
           '';
           default = git;
         };
