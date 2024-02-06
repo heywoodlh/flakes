8b04f090e5627178468195a75c264ef8b976b844
Spencer Heywood <github@heywoodlh.io>
2024-02-05 18:17:25 -0700

    updated fish flake.lock; suppress touch errors

--- fish/flake.nix
+++ fish/flake.nix
@@ -122,7 +122,7 @@
         # Always re-source ~/.config/fish/config.fish last
         # Prioritize local config
         mkdir -p ~/.config/fish
-        touch ~/.config/fish/config.fish
+        touch ~/.config/fish/config.fish &>/dev/null
         source ~/.config/fish/config.fish
       '';
     in {
