8425d5a021bbf8e6115fbdda33a1054b9c5f726e
Spencer Heywood <github@heywoodlh.io>
2024-09-10 11:30:19 -0600

    use MarceColl/zen-browser-flake if on Linux

--- zen-browser/flake.nix
+++ zen-browser/flake.nix
@@ -3,18 +3,21 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.zen-browser.url = "github:MarceColl/zen-browser-flake";
 
   outputs =
     inputs@{
       self,
       nixpkgs,
       flake-utils,
+      zen-browser,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        zenBrowser = pkgs.callPackage ./zen.nix {};
+        zenBrowser = if pkgs.stdenv.isDarwin then pkgs.callPackage ./zen.nix {}
+        else zen-browser.packages.${system}.default;
         zenWrapper = pkgs.writeShellScriptBin "zen" ''
           mkdir -p $HOME/Documents/zen/Profiles/main
           ${zenBrowser}/bin/zen --profile $HOME/Documents/zen/Profiles/main
@@ -24,7 +27,7 @@
         packages = rec {
           zen-wrapper = zenWrapper;
           zen-browser = zenBrowser;
-          default = zen-wrapper;
+          default = if pkgs.stdenv.isDarwin then zen-wrapper else zen-browser;
         };
 
         formatter = pkgs.nixfmt-rfc-style;
