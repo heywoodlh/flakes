999900b4cb09331faf99378f798d8f43e719f994
Spencer Heywood <github@heywoodlh.io>
2024-11-22 13:36:48 -0700

    updated vim clipboard to use xclip

--- vim/settings/os-specific.nix
+++ vim/settings/os-specific.nix
@@ -6,25 +6,18 @@
     " Yank to system clipboard with Ctrl + y
     noremap <silent> <C-y> "*y
   '' else ''
-    let $PATH = "${xclip}/bin:" . $PATH
-    " Wayland-specific config
-    if !empty($WAYLAND_DISPLAY)
-        let g:clipboard = {
-              \   'name': 'wayland-clip',
-              \   'copy': {
-              \      '+': '${wl-clipboard}/bin/wl-copy --foreground --type text/plain',
-              \      '*': '${wl-clipboard}/bin/wl-copy --foreground --type text/plain --primary',
-              \    },
-              \   'paste': {
-              \      '+': {-> systemlist('${wl-clipboard}/bin/wl-paste --no-newline | sed -e "s/\r$//"')},
-              \      '*': {-> systemlist('${wl-clipboard}/bin/wl-paste --no-newline --primary | sed -e "s/\r$//"')},
-              \   },
-              \   'cache_enabled': 1,
-              \ }
-    else
-        " X11-specific config
-    endif
-
+    let g:clipboard = {
+      \   'name': 'wayland-clip',
+      \   'copy': {
+      \      '+': '${xclip}/bin/xclip -sel clipboard',
+      \      '*': '${xclip}/bin/xclip -sel clipboard',
+      \    },
+      \   'paste': {
+      \      '+': {-> systemlist('${xclip}/bin/xclip -sel clipboard -o')},
+      \      '*': {-> systemlist('${xclip}/bin/xclip -sel clipboard -o')},
+      \   },
+      \   'cache_enabled': 1,
+      \ }
     " Yank to system clipboard with Ctrl + y
     noremap <silent> <C-y> "+y
   '';
