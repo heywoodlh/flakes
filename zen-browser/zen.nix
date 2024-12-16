1def883fb0ba63ac3ccb7c76f4e1274de0dcbd57
heywoodlh <heywoodlh@users.noreply.github.com>
2024-12-16 00:06:37 +0000

    GitHub Action automated Zen update

--- zen-browser/zen.nix
+++ zen-browser/zen.nix
@@ -13,10 +13,10 @@
 }:
 
 let
-  myZenVersion = "1.0.2-b.1";
+  myZenVersion = "1.0.2-b.2";
   x86_64-darwin-hash = "";
-  aarch64-darwin-hash = "1qrqdvakx2062fd1r4la2s71qrwyrpaj4b9hh8vfrawzzqxhw8r2";
-  x86_64-linux-hash = "1kcp2ignckh6ziqi7l1h0kn1g2bh1h80fy6xmxl6w9clihjcs4di";
+  aarch64-darwin-hash = "0dpanfzxjrq357swwk8g3f9fmij75dba5i6lhh88sy1mlwbgwwdg";
+  x86_64-linux-hash = "0pwnsi2j5hhcnsi89v9xrcjdy2q5cr6dchsaw2x4h0kj79a0ck47";
   sources = {
     x86_64-darwin = fetchurl {
       url = "https://github.com/zen-browser/desktop/releases/download/${myZenVersion}/zen.macos-x64.dmg";
