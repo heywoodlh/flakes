1eeace25a788354df65cd4c2cd812f91eb9592b3
Spencer Heywood <github@heywoodlh.io>
2025-04-27 10:42:59 -0600

    removed copilotchat configuration

--- vim/settings/copilot.nix
+++ vim/settings/copilot.nix
@@ -5,12 +5,4 @@
     copilot-vim
     CopilotChat-nvim
   ];
-  rc = ''
-    lua << EOF
-      require("CopilotChat").setup {
-        chat_autocomplete = false,
-        model = 'gpt-4o',
-      }
-    EOF
-  '';
 }
