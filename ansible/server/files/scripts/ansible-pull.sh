c65673e63c93e8e6d6871c77291821bb74621223
Spencer Heywood <github@heywoodlh.io>
2025-07-08 20:11:08 -0600

    conditional logic for nix-absent installations, added unifi support

--- ansible/server/files/scripts/ansible-pull.sh
+++ ansible/server/files/scripts/ansible-pull.sh
@@ -7,7 +7,7 @@ if command -v nix &>/dev/null
 then
   nix run "github:heywoodlh/flakes/$(git ls-remote https://github.com/heywoodlh/flakes | head -1 | awk '{print $1}')?dir=ansible#server"
 else
-  /opt/scripts/install-ansible.sh # install ansible in case it's been removed for some reason
+  command -v ansible &>/dev/null || /opt/scripts/install-ansible.sh # install ansible in case it's been removed for some reason
   mkdir -p /opt/ansible/
   curl --silent -L https://raw.githubusercontent.com/heywoodlh/flakes/main/ansible/requirements.yml -o /opt/ansible/requirements.yml
   ansible-galaxy install -r /opt/ansible/requirements.yml
