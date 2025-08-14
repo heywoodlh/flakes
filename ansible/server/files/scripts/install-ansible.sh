e8d021610f1757fec5ce26b4ac87f76756e155a7
Spencer Heywood <github@heywoodlh.io>
2025-08-14 14:59:14 -0600

    better support for older debian version on Unifi

--- ansible/server/files/scripts/install-ansible.sh
+++ ansible/server/files/scripts/install-ansible.sh
@@ -8,7 +8,13 @@ export PATH="$HOME/.local/bin:$PATH"
 if [[ -e /etc/debian_version ]]
 then
   apt update
-  apt install -y pipx git curl
+  # Install pipx via apt -- otherwise assume older Debian/Ubuntu version
+  # Older installs of Debian/Ubuntu do not have pipx available in apt
+  if ! apt install -y pipx git curl
+  then
+    apt install -y python3-pip git curl python3-venv
+    pip3 install --user pipx
+  fi
 
   dpkg -r ansible || true
   command -v ansible &>/dev/null || pipx install --include-deps ansible
